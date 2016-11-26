15da6371e3c7a849eb7eef57aaa19cb881515d6c
vdutor <dutordoirv@gmail.com>
2016-11-26 16:08:04 +0800

    collect laser info in cpp and python

---
+++ src/laser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import LaserScan
+
+def callback(data):
+    print "hello"
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("scan", LaserScan, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
