2abef0d80f3c8c8ebed94f149782ebd80b1fef9f
vdutor <dutordoirv@gmail.com>
2016-12-04 13:26:46 +0800

    start convert laser data to 2D array

--- src/laser.cpp
+++ src/laser.cpp
@@ -11,19 +11,19 @@ static const uint32_t MY_ROS_QUEUE_SIZE = 1000;
 void laser_cb(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     cout << endl << "Laser Message :" << endl;
-  cout << "angle_min: " << msg->angle_min << endl;
-  cout << "angle_max:  " << msg->angle_max << endl;
-  cout << "angle_inc: " << msg->angle_increment << endl;
-  cout << "scan_time: " << msg->scan_time << endl;
-  cout << "time_inc: " << msg->time_increment << endl;
-  cout << "range_min: " << msg->range_min << endl;
-  cout << "range_max: " << msg->range_max << endl;
-  
-  vector<float> ranges = msg->ranges;
+    cout << "angle_min: " << msg->angle_min << endl;
+    cout << "angle_max:  " << msg->angle_max << endl;
+    cout << "angle_inc: " << msg->angle_increment << endl;
+    cout << "scan_time: " << msg->scan_time << endl;
+    cout << "time_inc: " << msg->time_increment << endl;
+    cout << "range_min: " << msg->range_min << endl;
+    cout << "range_max: " << msg->range_max << endl;
 
-  cout << "ranges size: " << ranges.size() << endl;
-  int center_idx = ranges.size() / 2;
-  cout << "range[0]: " << ranges.at(center_idx) << endl; 
+    vector<float> ranges = msg->ranges;
+
+    cout << "ranges size: " << ranges.size() << endl;
+    int center_idx = ranges.size() / 2;
+    cout << "range[0]: " << ranges.at(center_idx) << endl;
 }
 
 int main(int argc, char* argv[])
--- src/laser.py
+++ src/laser.py
@@ -3,12 +3,63 @@ import rospy
 import math
 from sensor_msgs.msg import LaserScan
 
+from enum import Enum
+import cv2
+import time
+import pandas as pd
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
 x = 0
 y = 0
+
+class LaserData:
+    angle_min = -2.35619
+    angle_max = 2.09235
+    angle_increment = 0.00613592
+    scan_time = 0.1
+    range_min = 0.02
+    range_max = 5.6
+    ranges_size = 726
+    time_inc = 9.76563e-5
+    def __init__(self, ranges):
+        self.ranges = ranges
+
+class State(Enum):
+    full = 255
+    empty = 0
+
+class World:
+
+    def __init__(self, size):
+        self.world = np.zeros((size, size))
+        self.center = (size/2, size/2)
+        self.world[self.center] = State.full.value
+
+    def process_laser_data(self, data):
+        x,y = get_robot_position()
+        angles = [data.angle_min + idx * data.angle_increment for idx in range(len(data.ranges))]
+        obstacle_x = [x + (math.cos(angle) * dist) for angle,dist in zip(angles, data.ranges)]
+        obstacle_y = [y + (math.sin(angle) * dist) for angle,dist in zip(angles, data.ranges)]
+        # points =
+        # plt.ion()
+        # plt.scatter(*zip(*zip(obstacle_x, obstacle_y)))
+        # plt.pause(0.5)
+
+    def show(self):
+        scaled_world = cv2.resize(self.world, (600, 600))
+        cv2.imshow('frame', scaled_world)
+        cv2.waitKey(0)
+
+    def _add_data(self, points):
+        print 'e'
+
+
+def read_csv():
+    df = pd.read_csv("~/catkin_ws/laser_data.csv", delimiter=";", header=0)
+    return df
+
 
 def get_robot_position():
     global x,y
@@ -22,16 +73,6 @@ def store_measurements(data):
         fileWriter.writerow(data.ranges)
 
 
-def process_laser(data):
-    x,y = get_robot_position()
-    angles = [data.angle_min + idx * data.angle_increment for idx in range(len(data.ranges))]
-    obstacle_x = [x + (math.cos(angle) * dist) for angle,dist in zip(angles, data.ranges)]
-    obstacle_y = [y + (math.sin(angle) * dist) for angle,dist in zip(angles, data.ranges)]
-
-    plt.scatter(obstacle_x, obstacle_y)
-    plt.show()
-
-
 def init():
     rospy.init_node('laser node', anonymous=True)
     rospy.Subscriber("scan", LaserScan, process_laser)
@@ -39,5 +80,14 @@ def init():
     rospy.spin()
 
 if __name__ == '__main__':
-    init()
-
+    # init()
+    w = world()
+    df = read_csv()
+    ctr = 0
+    while ctr < 1: #len(df): TODO change this back
+        data = LaserData(df.loc[ctr])
+        print "processing line: ", ctr
+        w.process_laser_data(data)
+        w.show()
+        # time.sleep(3)
+        ctr += 1
