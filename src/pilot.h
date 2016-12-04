b31166a3c3287e0dc55633f166143541128d36cb
jonas <jonas@spanoghe.net>
2016-12-04 12:57:06 +0800

    Work on pilot to drive the robot

---
+++ src/pilot.h
@@ -0,0 +1,51 @@
+#ifndef RSC_PILOT
+#define RSC_PILOT
+
+#include "global.h"
+#include "odometry.h"
+#include <ros/ros.h>
+
+class Pilot
+{
+public:
+    Pilot();
+    Pilot(ros::Publisher serialCommandMsg, int speed, int wheelRadius);
+
+    /*
+     * Sends the "en" message to the robot
+     */
+    void enableRobot();
+
+    /*
+     * Moves the robot in a straight line
+     * length: the distance to traverse, in meters
+     * direction: 1 => forward
+     *           -1 => backward
+     */
+    void move(int direction, double length);
+
+    /*
+     * Rotates the robot
+     * angle: the angle the robot should rotate, in radians
+     * direction: 1 => positive rotation (to the right)
+     *           -1 => negative rotattion (to the left)
+     */
+    void rotate(int direction, double angle);
+
+    /*
+     * Sets the speed of both wheels to 0
+     */
+    void stopRobot();
+
+private:
+    void setLSpeed(int rotSpeed);
+    void setRSpeed(int rotSpeed);
+
+    Odometry odometry;
+    ros::Publisher serialPub;
+    int speed; // speed of the robot in m/s
+    int rotSpeed; // speed of the wheels in rotations/min
+    int wheelRadius;
+};
+
+#endif
