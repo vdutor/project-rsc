3aaa5b7960d8cc57f09ecaed3baf69f0f595fad7
vdutor <dutordoirv@gmail.com>
2016-12-18 17:16:29 +0800

    minor changes in wallfollowing

--- src/wallfollowing.cpp
+++ src/wallfollowing.cpp
@@ -1,6 +1,22 @@
 #include "wallfollowing.hpp"
 #include <math.h>
+#include <iostream>
+
 #define PI 3.141592
+#define SUB_BUFFER_SIZE 1  // Size of buffer for subscriber.
+#define PUB_BUFFER_SIZE 1000  // Size of buffer for publisher.
+#define WALL_DISTANCE .15
+#define MAX_SPEED 5
+#define P_DEFAULT 10    // Proportional constant for controller
+#define D_DEFAULT 5     // Derivative constant for controller
+#define ANGLE_COEF 1    // Proportional constant for angle controller
+#define DIRECTION 1 // 1 for wall on the left side of the robot (-1 for the right side).
+#define PUB_TOPIC "/cmd_vel"
+#define SUB_TOPIC "/scan"
+#define TO_DEGREE(r) (((r)/PI) * 180.0)
+#define DEBUG
+
+using namespace std;
 
 WallFollowing::WallFollowing(ros::Publisher pub, double wallDist, double maxSp, int dir, double pr, double di, double an)
 {
@@ -25,7 +41,7 @@ void WallFollowing::publishMessage()
     //preparing message
     geometry_msgs::Twist msg;
 
-    msg.angular.z = direction*(P*e + D*diffE) + angleCoef * (angleMin - PI*direction/2);    //PD controller
+    msg.angular.z = direction*(P*e + D*diffE) + angleCoef * (angleMin - PI*direction/2);
 
     if (distFront < wallDistance){
         msg.linear.x = 0;
@@ -40,6 +56,11 @@ void WallFollowing::publishMessage()
         msg.linear.x = maxSpeed;
     }
 
+#ifdef DEBUG
+    cout << "angle vel: " << msg.angular.z;
+    cout << " speed: " << msg.linear.x << endl;
+#endif
+
     //publishing message
     pubMessage.publish(msg);
 }
@@ -47,6 +68,7 @@ void WallFollowing::publishMessage()
 //Subscriber
 void WallFollowing::messageCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
+    e = 0;
     //vector<float> ranges = msg->ranges;
     int size = msg->ranges.size();
 
@@ -57,7 +79,7 @@ void WallFollowing::messageCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     //This cycle goes through array and finds minimum
     for(int i = minIndex; i < maxIndex; i++)
     {
-        if (msg->ranges.at(i) < msg->ranges.at(minIndex) && msg->ranges.at(i) > 0.0){
+        if (msg->ranges.at(i) < msg->ranges.at(minIndex) && msg->ranges.at(i) > 0.10){
             minIndex = i;
         }
     }
@@ -70,20 +92,17 @@ void WallFollowing::messageCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
     diffE = (distMin - wallDistance) - e;
     e = distMin - wallDistance;
 
+#ifdef DEBUG
+    cout << "angleMin: " << TO_DEGREE(angleMin);
+    cout << " distMin: " << distMin;
+    cout << " distFront: " << distFront;
+    cout << " error: " << e << endl;
+#endif
+
     //Invoking method for publishing message
     publishMessage();
 }
 
-#define SUBSCRIBER_BUFFER_SIZE 1  // Size of buffer for subscriber.
-#define PUBLISHER_BUFFER_SIZE 1000  // Size of buffer for publisher.
-#define WALL_DISTANCE 0.7
-#define MAX_SPEED 0.1
-#define P 10    // Proportional constant for controller
-#define D 5     // Derivative constant for controller
-#define ANGLE_COEF 1    // Proportional constant for angle controller
-#define DIRECTION -1 // 1 for wall on the left side of the robot (-1 for the right side).
-#define PUBLISHER_TOPIC "/cmd_vel"
-#define SUBSCRIBER_TOPIC "/scan"
 
 int main(int argc, char **argv)
 {
@@ -92,14 +111,14 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     //Creating publisher
-    ros::Publisher pubMessage = n.advertise<geometry_msgs::Twist>(PUBLISHER_TOPIC, PUBLISHER_BUFFER_SIZE);
+    ros::Publisher pubMessage = n.advertise<geometry_msgs::Twist>(PUB_TOPIC, PUB_BUFFER_SIZE);
 
     //Creating object, which stores data from sensors and has methods for
     //publishing and subscribing
-    WallFollowing *follower = new WallFollowing(pubMessage, WALL_DISTANCE, MAX_SPEED, DIRECTION, P, D, 1);
+    WallFollowing *follower = new WallFollowing(pubMessage, WALL_DISTANCE, MAX_SPEED, DIRECTION, P_DEFAULT, D_DEFAULT, 1);
 
     //Creating subscriber and publisher
-    ros::Subscriber sub = n.subscribe(SUBSCRIBER_TOPIC, SUBSCRIBER_BUFFER_SIZE, &WallFollowing::messageCallback, follower);
+    ros::Subscriber sub = n.subscribe(SUB_TOPIC, SUB_BUFFER_SIZE, &WallFollowing::messageCallback, follower);
     ros::spin();
 
     return 0;
